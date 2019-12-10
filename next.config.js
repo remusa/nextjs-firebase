aa2deabd9ab78e942ebc84b535cf7d80b9456a4b
R Sánchez <rene.as.himself@gmail.com>
2019-12-10 12:21:45 -0600

    Removed unused files

--- next.config.js
+++ next.config.js
@@ -1,25 +1,7 @@
 // const fetch = require('isomorphic-unfetch');
 const withOffline = require('next-offline')
-// import {firestore} from './lib/firebase'
-const env = require('./.env')
 
 const nextConfig = {
-  // exportTrailingSlash: true,
-  // exportPathMap: async function() {
-  //   const paths = {
-  //     '/': { page: '/' },
-  //     '/about': { page: '/about' }
-  //   }
-
-  //   const snapshot = await firestore.collection('entries').get()
-
-  //   snapshot.forEach(doc => {
-  //     const entry = { ...doc.data(), id: doc.id }
-  //     paths[`/product/${entry.id}`] = { page: '/product/[id]', query: { id: entry.id } }
-  //   })
-
-  //   return paths
-  // },
   env: env,
   target: 'serverless',
 	workboxOpts: {
@@ -35,13 +17,6 @@ const nextConfig = {
 					}
 				}
 			},
-			// {
-			// 	urlPattern: /^https:\/\/code\.getmdl\.io.*/,
-			// 	handler: 'CacheFirst',
-			// 	options: {
-			// 		cacheName: 'lib-cache'
-			// 	}
-			// },
 			{
 				urlPattern: /^http.*/,
 				handler: 'NetworkFirst',
