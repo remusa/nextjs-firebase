fce0e6c96d7aeb6e2344ad4a1e4694a8a1c6d4fd
R Sánchez <rene.as.himself@gmail.com>
2019-12-11 14:05:07 -0600

    dotenv

--- next.config.js
+++ next.config.js
@@ -3,26 +3,34 @@ const withOffline = require('next-offline')
 // const nextEnv = require('next-env')
 // const dotenvLoad = require('dotenv-load')
 // const withPlugins = require('next-compose-plugins')
+require('dotenv').config()
 
 // dotenvLoad()
 
-const dotEnvResult = require('dotenv').config()
+// const dotEnvResult = require('dotenv').config()
 
-if (dotEnvResult.error) {
-  throw dotEnvResult.error
-}
+// if (dotEnvResult.error) {
+//   throw dotEnvResult.error
+// }
 
-const parsedVariables = dotEnvResult.parsed || {}
-const dotEnvVariables = {}
-// We always want to use the values from process.env, since dotenv
-// has already resolved these correctly in case of overrides
-for (const key of Object.keys(parsedVariables)) {
-  dotEnvVariables[key] = process.env[key]
-}
+// const parsedVariables = dotEnvResult.parsed || {}
+// const dotEnvVariables = {}
+// for (const key of Object.keys(parsedVariables)) {
+//   dotEnvVariables[key] = process.env[key]
+// }
 
 const nextConfig = {
+  // env: {
+    //   ...dotEnvVariables,
+    // },
   env: {
-    ...dotEnvVariables,
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    DATABASE_URL: process.env.DATABASE_URL,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+    APP_ID: process.env.APP_ID,
   },
   target: 'serverless',
 	workboxOpts: {
