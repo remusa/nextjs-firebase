087f4bbe96b9b9a6e7a34f513d1768dedba22739
R Sánchez <rene.as.himself@gmail.com>
2019-12-11 13:40:42 -0600

    Use dotenv for loading .env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
 // const fetch = require('isomorphic-unfetch');
 const withOffline = require('next-offline')
+// const nextEnv = require('next-env')
+// const dotenvLoad = require('dotenv-load')
+// const withPlugins = require('next-compose-plugins')
+
+// dotenvLoad()
+
+const dotEnvResult = require('dotenv').config()
+
+if (dotEnvResult.error) {
+  throw dotEnvResult.error
+}
+
+const parsedVariables = dotEnvResult.parsed || {}
+const dotEnvVariables = {}
+// We always want to use the values from process.env, since dotenv
+// has already resolved these correctly in case of overrides
+for (const key of Object.keys(parsedVariables)) {
+  dotEnvVariables[key] = process.env[key]
+}
 
 const nextConfig = {
+  env: {
+    ...dotEnvVariables,
+  },
   target: 'serverless',
 	workboxOpts: {
 		swDest: 'public/service-worker.js',
@@ -38,3 +60,4 @@ const nextConfig = {
 }
 
 module.exports = withOffline(nextConfig)
+// module.exports = withPlugins([nextEnv(),], nextConfig)
